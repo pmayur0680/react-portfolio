e93fb0b17653e8754483c6378258a4594bfc81d6
Mayur Patel <pmayur0680@gmail.com>
2022-05-31 16:17:00 -0500

    contact form added

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className='container-fluid'>
+    <h2>Contact</h2>
+    <div className='backgroundbase rounded p-3 mt-2'>
+      <form>
+        <div className="form-group">
+          <label for="name" className="text-white">Name:</label>
+          <input type="text" className="form-control mt-2" id="name" />
+        </div>
+        <div className="form-group">
+          <label for="email" className="text-white">Email address:</label>
+          <input type="email" className="form-control mt-2" id="email" />
+        </div>
+        <div className="form-group">
+          <label for="message" className="text-white">Message</label>
+          <textarea class="form-control mt-2" id="message" rows="7"></textarea>
+        </div>
+        <button type="submit" className="btn btn-info mt-2">Submit</button>
+      </form>
+    </div>
     </div>
   );
 }
