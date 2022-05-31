4d86da26005aa26cd86c3bcd73d8077fe586509f
Mayur Patel <pmayur0680@gmail.com>
2022-05-31 18:53:08 -0500

    form handling and validation

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import helper function to validate email address
+import { validateEmail } from '../../utils/helpers';
 
 export default function Contact() {
+  // Create state variables for the fields in the form with initial values set to an empty string
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  // Create state variable to display validation error message
+  const [errorMessage, setErrorMessage] = useState('');
+  
+  const handleFormSubmit = (e) => {    
+    e.preventDefault();
+    
+    // validate form
+    if(!name) {
+      setErrorMessage('name is required!');
+      return;
+    }
+    if (!validateEmail(email)) {
+      setErrorMessage('Your email is Invalid!');
+      return;
+    }
+    if(!message) {
+      setErrorMessage('message is required!');
+      return;
+    }
+    // successful
+    setName('');
+    setEmail('');
+    setMessage('');
+    setErrorMessage('Thank you for reaching out to me.');
+  }  
   return (
     <div className='container-fluid'>
     <h2>Contact</h2>
@@ -8,18 +39,41 @@ export default function Contact() {
       <form>
         <div className="form-group">
           <label for="name" className="text-white">Name:</label>
-          <input type="text" className="form-control mt-2" id="name" />
+          <input 
+            type="text" 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="form-control mt-2" 
+            id="name" 
+          />
         </div>
         <div className="form-group">
           <label for="email" className="text-white">Email address:</label>
-          <input type="email" className="form-control mt-2" id="email" />
+          <input 
+            type="email" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control mt-2" 
+            id="email" 
+          />
         </div>
         <div className="form-group">
           <label for="message" className="text-white">Message</label>
-          <textarea class="form-control mt-2" id="message" rows="7"></textarea>
+          <textarea 
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            class="form-control mt-2" 
+            id="message" 
+            rows="7"            
+          />
         </div>
-        <button type="submit" className="btn btn-info mt-2">Submit</button>
+        <button type="button" className="btn btn-info mt-2" onClick={handleFormSubmit}>Submit</button>
       </form>
+      {errorMessage && (
+        <div>
+          <p className="mt-2 alert alert-warning" role="alert">{errorMessage}</p>
+        </div>
+      )}
     </div>
     </div>
   );
