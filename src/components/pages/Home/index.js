a43c16c16359dd37be6b4dd4d2be3421d48b9626
Mayur Patel <pmayur0680@gmail.com>
2022-06-13 16:14:12 -0500

    updated portfolio projects

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -10,14 +10,14 @@ export default function Home() {
         </div>
         <div className='container-fluid'>
           <h2>About me</h2>
-          <p>Full-stack web developer with a vast array of knowledge and 18 years of 
-          experience in different front end and back-end languages to build a more intuitive user experience 
-          on the web. Currently enrolled in full stack Coding Boot Camp at UT Austin, with newly developed skills 
-          in Advanced CSS, JavaScript, Node, Express, Sequelize, Handlebars, MVC paradigm, MongoDB, and React. 
-          Known as an innovative problem solver passionate about developing apps, with a focus on 
-          mobile-first design and development. With each project, my aim is to best engage my audience for 
-          an impactful user experience. I'm excited to leverage my skills as part of a fast-paced, 
-          quality-driven team to build better experiences on the web.</p>
+          <p>Full-stack web developer with a vast array of knowledge and 18 years
+of experience in front end and back-end languages to build more
+intuitive user experience on the web. Comes with a master's degree
+in computer science and a recently certified full stack web developer,
+bringing deep understanding and advanced knowledge of modern
+HTML, CSS, Bootstrap, JavaScript, NodeJS, React, REST API, GraphQL,
+PHP and more. Excited to leverage skills as part of a fast-paced,
+quality-driven team to build better experiences on the web.</p>
         </div>
     </div>
   );
--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -1,78 +1,125 @@
 import React from 'react';
+import fitterProject from './assets/images/fitter.png'
 import endlessTriviaProject from './assets/images/endless-trivia.png'
 import stockSimulatorProject from './assets/images/stock-simulator.png'
-import textEditorProject from './assets/images/text-editor.png'
-import contactDirectoryProject from './assets/images/contact-directory.png'
-import TechBlogProject from './assets/images/the-tech-blog.png'
-import noteTakerProject from './assets/images/note-taker.png'
+import ispProvidersInMyArea from './assets/images/ispprovidersinmyarea.png'
+import top10InUSA from './assets/images/top10inusa.png'
+import lawyersNearByMe from './assets/images/lawyersnearbyme.png'
+import freeWifiNearByMe from './assets/images/freewifinearbyme.png'
+import bestEnergyProviders from './assets/images/bestenergyproviders.png'
+import tvProvidersGuide from './assets/images/tvprovidersguide.png'
+import nrisworld from './assets/images/nrisworld.png'
+import topNewSmartphones from './assets/images/topnewsmartphones.png'
+import aiuwa from './assets/images/aiuwa.png'
+import pwrmd from './assets/images/pwrmd.png'
 
 function ProjectCard(props) {  
   return (    
-            <div className="col-6 card container_foto ">
+            <div className="col-6 card container_foto">              
               <div className="ver_mas text-center">
-                  <span className="lnr lnr-eye">
+                  <p className="lnr lnr-eye">
+                     <h4 style={{color: "#3e7184", fontWeight: "bold" }} className="mt-1">{props.title}</h4>
+                  {props.githubrepo &&                  
                     <a 
                       type="button" 
                       href={props.githubrepo} 
-                      className="btn btn-info"
-                    >GitHub</a>  <a 
+                      className="btn btn-light"
+                    >GitHub</a> }    <a 
                       type="button"
                       href={props.deployedurl} 
-                      className="btn btn-warning"
-                    >Live</a> 
-              </span>
+                      className="btn btn-secondary"
+                    >Live</a>
+              </p>
                   </div>
-                  <article className="text-left">
-                     <h2 className='text-black'>{props.title}</h2>
-                  </article>
-                  <img src={props.thumbnail} alt={props.title} />
+                  
+                  <img src={props.thumbnail} alt={props.title} />                  
+                  
                </div>
     );
 }
 export default function Portfolio() {  
     const Projects = [
-        {
+          {
             "id": 1,
+            "title": "Fiiter - Social Fitness Network",
+            "thumbnail": fitterProject,
+            "githubrepo": "https://github.com/yummy314159265/Fitter",
+            "deployedurl": "https://fitter-bc2022.herokuapp.com/"
+        },
+        {
+            "id": 2,
             "title": "Endless Trivia",
             "thumbnail": endlessTriviaProject,
             "githubrepo": "https://github.com/pmayur0680/Endless-Trivia",
             "deployedurl": "https://triviabymmaac.herokuapp.com/"
         },
         {
-            "id": 2,
+            "id": 3,
             "title": "Stock Simulator",
             "thumbnail": stockSimulatorProject,
             "githubrepo": "https://github.com/pmayur0680/Stock-Market-Simulator",
             "deployedurl": "https://pmayur0680.github.io/Stock-Market-Simulator/"
-        },
-        {
-            "id": 3,
-            "title": "PWA Text Editor",
-            "thumbnail": textEditorProject,
-            "githubrepo": "https://github.com/pmayur0680/pwa-text-editor",
-            "deployedurl": "https://jate-text-editor22.herokuapp.com/"
-        },
+        },        
         {
             "id": 4,
-            "title": "PWA Contact App",
-            "thumbnail": contactDirectoryProject,
-            "githubrepo": "https://github.com/pmayur0680/contact-directory-app",
-            "deployedurl": "https://intense-fortress-87690.herokuapp.com/"
-        },
+            "title": "ISP Providers",
+            "thumbnail": ispProvidersInMyArea,            
+            "deployedurl": "https://www.ispprovidersinmyarea.com/"
+        },   
         {
             "id": 5,
-            "title": "The Tech Blog",        
-            "thumbnail": TechBlogProject,        
-            "githubrepo": "https://github.com/pmayur0680/The-Tech-Blog",
-            "deployedurl": "https://serene-ridge-42390.herokuapp.com/"
+            "title": "Top 10 in USA",
+            "thumbnail": top10InUSA,            
+            "deployedurl": "https://www.top10inusa.com/"
         },   
         {
             "id": 6,
-            "title": "Note Taker",
-            "thumbnail": noteTakerProject,
-            "githubrepo": "https://github.com/pmayur0680/note-taker",
-            "deployedurl": "https://note-taker-0422.herokuapp.com/notes"
-        }    
+            "title": "Lawyers Near Me",
+            "thumbnail": lawyersNearByMe,            
+            "deployedurl": "http://www.lawyersnearbyme.com/"
+        },   
+        {
+            "id": 7,
+            "title": "Free WiFi Near Me",
+            "thumbnail": freeWifiNearByMe,            
+            "deployedurl": "http://www.freewifinearbyme.com/"
+        },   
+        {
+            "id": 8,
+            "title": "Compare Electricity Rates & Plans",
+            "thumbnail": bestEnergyProviders,            
+            "deployedurl": "https://www.bestenergyproviders.com/"
+        },   
+        {
+            "id": 9,
+            "title": "Compare Cable & Satellite TV Providers",
+            "thumbnail": tvProvidersGuide,            
+            "deployedurl": "https://www.tvprovidersguide.com/"
+        },   
+        {
+            "id": 10,
+            "title": "NRI's World",
+            "thumbnail": nrisworld,            
+            "deployedurl": "http://www.nrisworld.com/"
+        },   
+        {
+            "id": 11,
+            "title": "Compare smartphones specs & prices",
+            "thumbnail": topNewSmartphones,            
+            "deployedurl": "http://www.topnewsmartphones.com/"
+        },
+        {
+          "id": 12,
+          "title": "Student Mangement System - AIUWA",
+          "thumbnail": aiuwa,            
+          "deployedurl": "https://aiu.edu.gm/"
+        },       
+        {
+          "id": 13,
+          "title": "Electronic Medical Records (EMR)",
+          "thumbnail": pwrmd,            
+          "deployedurl": "https://www.pwrmd.com/"
+        }
     ]
   return (
     <div className= 'container-fluid'>
